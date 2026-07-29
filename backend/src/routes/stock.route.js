a4e67bb14c6c152aa9b0ffc1d85a28c3d386ec31
Ahmed El Shaf3y <aaalahmed1234@gmail.com>
2026-07-29 22:53:47 +0300

    Add getStockId route to stockRouter

--- backend/src/routes/stock.route.js
+++ backend/src/routes/stock.route.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const stockRouter = express.Router();
-const {getStock,addStock,updateStock,deleteStock} = require("../controller/stock.controller");
+const {getStock,addStock,updateStock,deleteStock,getStockId} = require("../controller/stock.controller");
 
+//route
+stockRouter.post("/", addStock);
 stockRouter.get("/", getStock);
+stockRouter.get("/:id", getStockId);
 stockRouter.put("/:id", updateStock);
 stockRouter.delete("/:id", deleteStock);
-stockRouter.post("/", addStock);
+
 
 module.exports = { stockRouter };
