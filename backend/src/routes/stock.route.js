0e90d451240256c332bede5ce832114e2b100940
Ahmed El Shaf3y <aaalahmed1234@gmail.com>
2026-08-02 05:24:33 +0300

    Add auth middleware to stock routes
    Updated stock routes to include authentication and authorization middleware.

--- backend/src/routes/stock.route.js
+++ backend/src/routes/stock.route.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const stockRouter = express.Router();
-const {
-  getStock,
-  addStock,
-  updateStock,
-  deleteStock,
-  getStockId,
-} = require("../controllers/stock.controller");
+const {getStock,getStockId} = require("../controllers/stock.controller");
+const { protect, authorize } = require('../middleware/auth');
 
 //route
-stockRouter.post("/", addStock);
-stockRouter.get("/", getStock);
-stockRouter.get("/:id", getStockId);
-stockRouter.put("/:id", updateStock);
-stockRouter.delete("/:id", deleteStock);
+// stockRouter.post("/", addStock);
+stockRouter.get("/",protect, authorize('admin'), getStock);
+stockRouter.get("/:id",protect, authorize('admin'), getStockId);
+// stockRouter.put("/:id", updateStock);
+// stockRouter.delete("/:id", deleteStock);
+// stockRouter.delete("/", deleteAllStock);
 
-module.exports = stockRouter;
+
+module.exports = { stockRouter };
