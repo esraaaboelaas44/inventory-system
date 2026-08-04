7e3d1b46765901405780a079c656dbfd8bbc774b
PolaMankarious <148140467+PolaMankarious@users.noreply.github.com>
2026-08-05 01:05:55 +0300

    added detailed order in the path of order

--- frontend/inventory/src/app/app.routes.ts
+++ frontend/inventory/src/app/app.routes.ts
@@ -4,17 +4,24 @@ import { Login } from './features/auth/components/login/login';
 import { AddUpdateSupplier } from './features/supplier/add-update-supplier/add-update-supplier';
 import { Supplier } from './features/supplier/supplier';
 import { Order } from './features/order/order';
+import { DetailedOrder } from './features/order/detailed-order/detailed-order';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'products', component: ProductsList },
-  { path: 'supplier', children: [
-    { path: '', component: Supplier },
-    { path: 'add-update-supplier', component: AddUpdateSupplier },
-  ] },
+  {
+    path: 'supplier',
+    children: [
+      { path: '', component: Supplier },
+      { path: 'add-update-supplier', component: AddUpdateSupplier },
+    ],
+  },
   {
     path: 'order',
-    component: Order,
+    children: [
+      { path: '', component: Order },
+      { path: 'app-detailed-order', component: DetailedOrder },
+    ],
   },
 ];
