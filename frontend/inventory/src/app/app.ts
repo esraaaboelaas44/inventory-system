e2e9b47a2fc5b48bf35f340bd48182c7d7931bc8
esraaaboelaas44 <esraa.aboelaas44@gmail.com>
2026-08-04 19:01:55 +0300

    login merged successfully

--- frontend/inventory/src/app/app.ts
+++ frontend/inventory/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Login } from './features/auth/components/login/login';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Login],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
