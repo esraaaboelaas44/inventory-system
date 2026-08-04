18c8d6a57bee95cce70293a8bd722fac8ed59ae3
PolaMankarious <148140467+PolaMankarious@users.noreply.github.com>
2026-08-05 01:08:33 +0300

    refactored code

--- frontend/inventory/src/app/features/supplier/add-update-supplier/add-update-supplier.ts
+++ frontend/inventory/src/app/features/supplier/add-update-supplier/add-update-supplier.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { Sidebar } from '../../../shared/components/sidebar/sidebar';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-add-update-supplier',
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule, Sidebar],
   templateUrl: './add-update-supplier.html',
   styleUrls: ['./add-update-supplier.css'],
 })
