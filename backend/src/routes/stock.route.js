3c268937d7036d5d310b2d03d294e61c4e36c1b8
Ahmed El Shaf3y <aaalahmed1234@gmail.com>
2026-08-05 15:54:58 +0300

    Enhance stock routes with additional endpoints

--- backend/src/routes/stock.route.js
+++ backend/src/routes/stock.route.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const stockRouter = express.Router();
-const {getStock,getStockId} = require("../controllers/stock.controller");
+const {getStock,getStockId,getLowStockProducts,getAdd,getRemove,getUpdate,findStock} = require("../controllers/stock.controller");
 const { protect, authorize } = require('../middleware/auth');
 
 //route
-// stockRouter.post("/", addStock);
-stockRouter.get("/",protect, authorize('admin'), getStock);
-stockRouter.get("/:id",protect, authorize('admin'), getStockId);
-// stockRouter.put("/:id", updateStock);
-// stockRouter.delete("/:id", deleteStock);
-// stockRouter.delete("/", deleteAllStock);
+stockRouter.get("/",protect, authorize("Admin","Manager"),getStock);
+stockRouter.get("/:id",protect, authorize("Admin","Manager"), getStockId);
+stockRouter.get("/low",protect, authorize("Admin","Manager"), getLowStockProducts);   
+stockRouter.get("/add",protect, authorize("Admin","Manager"), getAdd);
+stockRouter.get("/update",protect, authorize("Admin","Manager"), getUpdate);
+stockRouter.get("/remove",protect, authorize("Admin","Manager"), getRemove);
+stockRouter.get("/find",protect, authorize("Admin","Manager"), findStock);
 
 
 module.exports = { stockRouter };
