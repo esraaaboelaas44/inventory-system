391e85bb4bb6e687377b8ffa12224f5e2bba43a4
Ahmed El Shaf3y <aaalahmed1234@gmail.com>
2026-08-06 02:04:24 +0300

    Add SKU parameter to createStock function

--- backend/src/utils/creatStock.js
+++ backend/src/utils/creatStock.js
@@ -1,9 +1,10 @@
 const { stockModel } = require("../models/stock.model");
 
-const createStock = async (productName,categoryId,quantity,newquantity,action,userID) => {
+const createStock = async (productName,sku,categoryId,quantity,newquantity,action,userID) => {
 
     return await stockModel.create({
         Name: productName,
+        sku:sku,
         category: categoryId,
         quantity:quantity,
         newquantity: newquantity,
