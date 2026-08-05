096590c6855bfd33c35011992c5e05105e875db0
PolaMankarious <148140467+PolaMankarious@users.noreply.github.com>
2026-08-05 18:50:11 +0300

    ngModel connecting form to backend

--- frontend/inventory/src/app/features/supplier/add-update-supplier/add-update-supplier.ts
+++ frontend/inventory/src/app/features/supplier/add-update-supplier/add-update-supplier.ts
@@ -2,11 +2,44 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Sidebar } from '../../../shared/components/sidebar/sidebar';
 import { FormsModule } from '@angular/forms';
+import { SupplierService } from '../../../../services/supplier.service';
+import { iSupplier } from '../../../models/iSupplier';
 @Component({
   selector: 'app-add-update-supplier',
   standalone: true,
-  imports: [CommonModule, Sidebar],
+  imports: [CommonModule, Sidebar, FormsModule],
   templateUrl: './add-update-supplier.html',
   styleUrls: ['./add-update-supplier.css'],
 })
-export class AddUpdateSupplier {}
+export class AddUpdateSupplier {
+  constructor(private SupplierService: SupplierService) {}
+
+  newSupplier: iSupplier = {
+    name: '',
+    industry: '',
+    email: '',
+    phone: '',
+    address: '',
+    contactPerson: {
+      name: '',
+      email: '',
+      phone: '',
+    },
+  };
+
+  addSupplier() {
+    this.SupplierService.addSupplier(this.newSupplier).subscribe({
+      next: (res: any) => {
+        console.log(res);
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('You successfully added a Supplier');
+        console.log(this.newSupplier);
+        alert('Supplier Added Successfully!');
+      },
+    });
+  }
+}
