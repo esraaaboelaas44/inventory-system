ac9b686d7eab43dddb1b917c888105ce5e550f6e
Ahmed El Shaf3y <aaalahmed1234@gmail.com>
2026-08-06 10:49:03 +0300

    Update stock.route.js

--- backend/src/routes/stock.route.js
+++ backend/src/routes/stock.route.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const stockRouter = express.Router();
-const {getStock,getStockId,getLowStockProducts,getAdd,getRemove,getUpdate,findStock} = require("../controllers/stock.controller");
+const {getStock,getLowStockProducts,deleteStock} = require("../controllers/stock.controller");
 const { protect, authorize } = require('../middleware/auth');
 
 //route
 stockRouter.get("/",protect, authorize("admin","manager"),getStock);
-stockRouter.get("/low",protect, authorize("admin","manager"), getLowStockProducts);   
-stockRouter.get("/add",protect, authorize("admin","manager"), getAdd);
-stockRouter.get("/update",protect, authorize("admin","manager"), getUpdate);
-stockRouter.get("/remove",protect, authorize("admin","manager"), getRemove);
-stockRouter.get("/find",protect, authorize("admin","manager"), findStock);
-stockRouter.get("/:id",protect, authorize("admin","manager"), getStockId);
-
+stockRouter.get("/low",protect, authorize("admin","manager"), getLowStockProducts);  
+stockRouter.delete("/:id",protect, authorize("admin","manager"), deleteStock);  
+ 
 
 module.exports = { stockRouter };
+
